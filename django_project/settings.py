864a03aeab1f6a856faef4fcbbd6bcf1d7d71caa
Ayim <ayim.shaimbetova@alatoo.edu.kg>
2024-10-28 00:48:36 +0600

    Chapter 13. Newspaper App

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
+    "articles.apps.ArticlesConfig", 
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+TIME_ZONE = "America/New_York"
