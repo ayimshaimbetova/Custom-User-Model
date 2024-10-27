9d9de747888bc6bc679a178e9cdb891dc4485b82
Ayim <ayim.shaimbetova@alatoo.edu.kg>
2024-10-28 01:19:59 +0600

    Chapter 15. Comments

--- articles/views.py
+++ articles/views.py
@@ -1,16 +1,33 @@
-from django.views.generic import ListView, DetailView  
+from django.views.generic import ListView, DetailView,  FormView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView 
-from django.urls import reverse_lazy  
+from django.urls import reverse_lazy, reverse
 from .models import Article
+from .forms import CommentForm
+from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.detail import SingleObjectMixin
 
 class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
 
-class ArticleDetailView(LoginRequiredMixin, DetailView):  
+class ArticleDetailView(LoginRequiredMixin, View):  
     model = Article
     template_name = "article_detail.html"
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
+
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):  
     model = Article
@@ -42,3 +59,32 @@ class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):  
         obj = self.get_object()
         return obj.author == self.request.user
+    
+class CommentGet(DetailView):  
+    model = Article
+    template_name = "article_detail.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+    
+
+class CommentPost(SingleObjectMixin, FormView):
+    model = Article
+    form_class = CommentForm
+    template_name = "article_detail.html"
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        article = self.get_object()
+        return reverse("article_detail", kwargs={"pk": article.pk})
